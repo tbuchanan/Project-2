5d30441aaa1f3d0492e82d692d43f186cb97e0dd
mathur7 <mathur7@gmail.com>
2014-05-23 15:28:44 -0700

    added create method, all routes

--- app/controllers/popups_controller.rb
+++ app/controllers/popups_controller.rb
@@ -2,7 +2,30 @@ class PopupsController < ApplicationController
 
 def index
   @popups = Popup.all
+  respond_to do |f|
+    f.html { render :index }
+    f.json { render json: @popups, :only => [:name, :address, :hours, :expires_at, :price, :description, :image] }
+  end
+end
+
+def create
+  @popup = Popup.new popup_params
+  if @popup.save
+    respond_to do |f|
+      f.html { render :index }
+      f.json { render json: @popup, status: :created }
+    end
+  else
+    f.json { render json: @popup.errors, status: :unprocessable_entity }
+  end
+end
+
+private 
+
+def popup_params
+  params.require(:popup).permit(:name, :address, :hours, :expires_at, :price, :description, :image)  
+end
+
 end
 
 
-end
