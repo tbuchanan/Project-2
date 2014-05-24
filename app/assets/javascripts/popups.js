08e89a6b97dd6f1b2c82ffc3a9d2267165a257b4
mathur7 <mathur7@gmail.com>
2014-05-24 16:37:05 -0700

    got ajax post working

--- app/assets/javascripts/popups.js
+++ app/assets/javascripts/popups.js
@@ -1,44 +1,47 @@
+ $(document).ready(function(){
  var loadPopups = function() {
     $.ajax('/popups.json', {
       type: 'get'
     }).success(function(data) {
       for (var i in data) {
         $('#popups').append(
-        	'<li>' + data[i]["name"],
-        	 '<br>' + data[i]["description"],
-    		 '<br>' + data[i]["address"],
-    		 '<br>' + data[i]["hours"],
-    		 '<br>' + data[i]["expires_at"] + '</li>')
+        	'<li>' + data[i].name,
+        	'<br>' + data[i].description,
+    		 '<br>' + data[i].address,
+    		 '<br>' + data[i].hours,
+    		 '<br>' + data[i].expires_at + '</li>');
       }
     });
-  }
+  };
   loadPopups();
 
- function addPopup(name){
+
+ function addPopup(name, description){
   $('#submit').click(function(e){
     e.preventDefault();
     var name = $("#name").val();
     $('#popups').append("<li>" + name + "</li>");
     // $("#name").val("");
+   
 
-
-    $.ajax({
-      url: ('/popups/create'),
-      method: ('post'),
-      data:   {
-        "popup": {
-          "name": name
-        		 }
-      		  }
-  			},
-      dataType: "json",
-      success: function(data) {
-        console.log(data);
-      						  }
- 								}
- 							};
- 									
-addPopup();
+  $.ajax({
+  url: ('/popups'),
+  type: ('post'),
+  data: {
+    "popup": {
+      "name": name
+    }
+  },
+  dataType: "json",
+  success: function(data) {
+    addPopup(data.name);
+    console.log(data);
+  }
+   });
+});
+  }
+  addPopup();
+ });
 
 
 
