7d26fbfad3625ecf098f4267e9165f570212054c
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-05-24 15:40:53 -0700

    addPopup function working w/o ajax post request. next step ajax post

--- app/assets/javascripts/popups.js
+++ app/assets/javascripts/popups.js
@@ -1,42 +1,28 @@
-$(document).ready(function() {
-    var loadPopups = function() {
-      $.ajax('/popups.json', {
-        type: 'get'
-      }).success(function(data) {
-        for (var i in data) {
-          $('#popups').append(
-            '<li>' + data[i]["name"],
-            '<br>' + data[i]["description"],
-            '<br>' + data[i]["address"],
-            '<br>' + data[i]["hours"],
-            '<br>' + data[i]["expires_at"] + '</li>')
-        }
-      });
-    }
-    loadPopups();
-
-    function addPopup(name) {
-      $('#submit').click(function(e) {
-          e.preventDefault();
-          var name = $("#name").val();
-          $('#popups').append("<li>" + name + "</li>");
-          // $("#name").val("");
+ $(document).ready(function() {
+   var loadPopups = function() {
+     $.ajax('/popups.json', {
+       type: 'get'
+     }).success(function(data) {
+       for (var i in data) {
+         $('#popups').append(
+           '<li>' + data[i].name + '</li>',
+           '<li>' + data[i].description + '</li>',
+           '<li>' + data[i].address + '</li>',
+           '<li>' + data[i].hours + '</li>',
+           '<li>' + data[i].expires_at + '</li>');
+       }
+     });
+   };
+   loadPopups();
 
 
-          $.ajax({
-              url: ('/popups/create'),
-              method: ('post'),
-              data: {
-                "popup": {
-                  "name": name
-                }
-              }
-            },
-            dataType: "json",
-            success: function(data) {
-              console.log(data);
-            }
-          }
-        };
-
-        addPopup();
+   function addPopup() {
+     $('#submit').click(function(e) {
+       e.preventDefault();
+       var name = $("#name").val();
+       $('#popups').append("<li>" + name + "</li>");
+       $("#name").val("");
+     });
+   }
+   addPopup();
+ });
