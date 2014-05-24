430e68e97828871c30eb2b97e0c662b6835dd7f6
mathur7 <mathur7@gmail.com>
2014-05-24 11:32:34 -0700

    added seed data and fixed syntax error in popup model

--- app/models/popup.rb
+++ app/models/popup.rb
@@ -1,6 +1,6 @@
 class Popup < ActiveRecord::Base
 
   belongs_to :user
-  has_many :comments, through :users
+  has_many :comments, through: :users
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Popup.create([{name: "James' Jeans", address: "123 main st, san francisco,ca ", description: "get cheap jeans", price: "$", hours: "3:00 pm - 6:00 pm", image: "jeans.jpg", expires_at: "June 1, 2014"}])
+Popup.create([{name: "Tim's Tacos", address: "456 hunger st, san francisco,ca ", description: "get cheap tacos", price: "$", hours: "3:00 pm - 6:00 pm", image: "tacos.jpg", expires_at: "June 1, 2014"}])
+Popup.create([{name: "Miley's Boutique", address: "404 forbidden st, san francisco,ca ", description: "awful designer clothing", price: "$$$", hours: "3:00 pm - 9:00 pm", image: "miley.jpg", expires_at: "June 4, 2014"}])
+Popup.create([{name: "Breakback Records", address: "200 music st, san francisco,ca ", description: "get free music records", price: "$", hours: "10:00 am - 11:00 am", image: "record.jpg", expires_at: "June 2, 2014"}])
