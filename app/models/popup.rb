4fd8c80f75d881738c26f37a68284aa064304362
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-05-27 17:17:07 -0700

    fixed paperclip error

--- app/models/popup.rb
+++ app/models/popup.rb
@@ -22,7 +22,7 @@ class Popup < ActiveRecord::Base
 
 
   # Validate the attached image is image/jpg, image/png, etc
-  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def self.search_for(query)
     where('name LIKE :query', query: "%#{query}%")
