4a5120e6eea7ece914f32f01defc15553da391fc
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-05-29 16:31:47 -0700

    added to readme including heroku app

--- app/models/popup.rb
+++ app/models/popup.rb
@@ -3,7 +3,13 @@ class Popup < ActiveRecord::Base
 
   belongs_to :user
   has_many :feeds
+  before_save { |popup| popup.name = name.downcase }
 
+  before_save do |popup|
+    if popup.expires_at == nil
+      popup.expires_at += 
+    end
+  end
   # validates :image, :presence => { :message => "Image is required" }
 
   validates :geocode, presence: true
@@ -21,6 +27,7 @@ class Popup < ActiveRecord::Base
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 
   def self.search_for(query)
     where('name LIKE :query', query: "%#{query}%")
