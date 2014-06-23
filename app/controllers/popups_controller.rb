f11ef739a591010c7e0fffcf4e4568ec10c23aeb
mathur7 <mathur7@gmail.com>
2014-06-23 00:05:51 -0700

    some stylistic changes, cleaned up controller and added code for current user in show method, made some changes to overall layout

--- app/controllers/popups_controller.rb
+++ app/controllers/popups_controller.rb
@@ -3,37 +3,32 @@ class PopupsController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    # @popups = Popup.search_for(params[:q]).where(active: true)
     @popups = Popup.all
     respond_to do |f|
       f.html { render :index }
       f.json { render json: @popups, :only => [:id, :name, :address, :geocode, :longitude, :latitude, :hours, :day, :website, :price, :description, :image, :category]}
     end
   end
-  
-  def create
-    @popup = Popup.new popup_params 
-    if @popup.save
-      respond_to do |f|
-        f.html { render :index }
-        f.json { render json: @popup, status: :created }
-      end
-    else
-      f.json { render json: @popup.errors, status: :unprocessable_entity }
-    end
-  end
 
   def search
     @results = Popup.search_for(params[:q]) || Popup.near(params[:geocode])
-    # render json: @results, status: :ok
+  end
+  
+  def new
+    @popup = Popup.new
+  end
+
+  def create
+    @popup = Popup.create popup_params 
+    redirect_to popup_path(@popup)
   end
 
   def show    
     @popup = Popup.find(params[:id])
     @feed_new = Feed.new
+    @user = current_user
     @feeds = @popup.feeds.all
   end
-
 
   def update
     @popup = Popup.find(params[:id])
