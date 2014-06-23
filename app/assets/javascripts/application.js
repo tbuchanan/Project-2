b1e4134e6849b408ce1d56ee8345bb4f2d434947
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-06-23 14:22:01 -0700

    fixed google map bugs on homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,37 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap.min
 //= require handlebars.runtime
 //= require_tree .
 
-var ready = function() {
-  // var loadPopups = function() {
-  //   $.ajax('/popups.json', {
-  //     type: 'get',
-  //     data: {
-  //       "q": $('#q').val()
-  //     },
-  //   }).success(function(data) {
-  //     for (var i in data) {
-  //       var exp = new Date(data[i].expires_at);
-  //       // console.log(data[i]);
-
-  //       $('#popups').append(
-  //         '<a href=/popups/' + data[i].id + '>' + '<li>' + '<br>' + data[i].name,
-  //         data[i].description, '</br>',
-  //         data[i].address, '</br>',
-  //         data[i].hours, '</br>',
-  //         data[i].price, '</br>',
-  //         exp.toLocaleDateString(), '</br>',
-  //         '<a href=' + data[i].website + '>' + data[i].website + '</a>' + '</br>' + '</li></a>');
-  //     }
-  //   });
-  // };
-  // loadPopups();  
-  
-  (function initialize() {
+$(document).ready(function() {
+    function initialize() {
     //just a variable storing a location
     var mapOptions = {
       center: new google.maps.LatLng(37.776616, -122.416972),
@@ -51,9 +26,10 @@ var ready = function() {
     // mapTypeId: google.maps.MapTypeId.ROADMAP,
     // styles: [{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#000000"}]},{"featureType":"landscape","stylers":[{"color":"#ffffff"},{"visibility":"on"}]},{}]
     };
+    var map = new google.maps.Map(document.getElementById("map-canvas-index"),
+    mapOptions);
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
+
     // alert(gon.current_user)
     var loadGeo = function() {
       var url = "/popups.json";
@@ -67,29 +43,6 @@ var ready = function() {
     };
     loadGeo();
 
-// pins are showing up on top of all pins. need to clear map before search
-    // var loadSearch = function() {
-    //   // Find search string and geocode and add to url
-    //   var url = "/popups/search.json";
-    //   $.ajax(url, {
-    //     type: 'get',
-    //     data: {
-    //     "q": $('#q').val(),
-    //     "geocode": $('#geocode').val()
-    //   }
-    //   }).success(function(data) {
-    //     for (var i in data) {
-    //       addPin(data[i].latitude, data[i].longitude, data[i].name, data[i].id);
-    //     }
-    //   });
-    // };
-
-    // $('#search_form').on('submit', function(event) {
-    //   // event.preventDefault();
-    //   loadSearch();
-    // })
-
-  
     function addPin(latitude, longitude, name, id) {
       var loc = new google.maps.LatLng(latitude, longitude);
       console.log(loc);
@@ -120,8 +73,6 @@ var ready = function() {
       });
     }
 
-  })();
-
-};
-$(document).ready(ready);
-$(document).on('page:load', ready);
+  };
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
