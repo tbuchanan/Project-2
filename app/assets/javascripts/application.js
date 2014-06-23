75b3957315b437a7d8eb599d007e6f3ba14841bd
mathur7 <mathur7@gmail.com>
2014-06-23 16:21:10 -0700

    added more info in infowindow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,7 +62,7 @@ var ready = function() {
         type: 'get'
       }).success(function(data) {
         for (var i in data) {
-          addPin(data[i].latitude, data[i].longitude, data[i].name, data[i].id);
+          addPin(data[i].latitude, data[i].longitude, data[i].name, data[i].id, data[i].address, data[i].category);
         }
       });
     };
@@ -91,22 +91,46 @@ var ready = function() {
     // })
 
   
-    function addPin(latitude, longitude, name, id) {
+    function addPin(latitude, longitude, name, id, address, category) {
       var loc = new google.maps.LatLng(latitude, longitude);
       console.log(loc);
       var newMarker = new google.maps.Marker({
         position: loc,
         map: map,
         animation: google.maps.Animation.DROP,
+        icon: "http://i.imgur.com/7sKFaCa.png",
         title: name
       });
-      var contentString = "<a href=/popups/" + id + '>' + name + "</a>" ;
+
+    //   infobox = new InfoBox({
+    //      content: document.getElementById("infobox"),
+    //      disableAutoPan: false,
+    //      maxWidth: 150,
+    //      pixelOffset: new google.maps.Size(-140, 0),
+    //      zIndex: null,
+    //      boxStyle: {
+    //         background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/examples/tipbox.gif') no-repeat",
+    //         opacity: 0.75,
+    //         width: "280px"
+    //     },
+    //     closeBoxMargin: "12px 4px 2px 2px",
+    //     closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+    //     infoBoxClearance: new google.maps.Size(1, 1)
+    // });
+    
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     infobox.open(map, this);
+    //     map.panTo(loc);
+    // });
+
+      var contentString = "<a href=/popups/" + id + '>' + name + "</a>" + '<br>' + address + '<br>' + category + '<br>' ;
       
       var newInfoWindow = new google.maps.InfoWindow({
         content: contentString
       });
       addInfoWindowListener(newMarker, newInfoWindow);
     };
+
 
 
     var lastInfoWindow;
