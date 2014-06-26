730d67a856f186578620a8baeb46b82c98e56424
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-06-25 18:46:18 -0700

    commit before group merge

--- app/models/popup.rb
+++ app/models/popup.rb
@@ -1,6 +1,5 @@
 class Popup < ActiveRecord::Base
 
-
   belongs_to :user
   has_many :feeds
 
@@ -20,8 +19,8 @@ class Popup < ActiveRecord::Base
   has_attached_file :image, styles: {
       thumb: '100x100>',
       square: '200x200#',
-      medium: '300x300>'
-     }
+      medium: '400x266>'
+  }
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
