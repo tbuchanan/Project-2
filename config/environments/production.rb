bacabcea0afc765892e8b9a63c6148abd7e8b206
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-07-04 15:52:38 -0700

    setup email through devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,19 @@ Rails.application.configure do
 
   # for devise use on Heroku
   config.action_mailer.default_url_options = { :host => 'http://popups-app.herokuapp.com/' }
+
+  # devise email setup
+  config.action_mailer.delivery_method = :smtp 
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
+
